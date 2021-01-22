9c7d1903853448311e5be7aac66928b699e417a2
tyson9tech <70201767+tyson9tech@users.noreply.github.com>
2021-01-22 13:07:26 -0500

    Premiere partie HTML et CSS Du Projet Tyson9Tech Group Chat avec SASS

---
+++ application/pages/homechat.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="../../css/styles.css">
+</head>
+
+<body class="pages">
+	<aside id="leftSection">
+		<div id="userProfile">
+			<div id="userImage"></div>
+			<div id="userName">
+				Tyson9Tech
+			</div>
+			<div id="userMail">
+				tyson9tech@gmail.com
+			</div>
+		</div>
+
+		<ul id="users">
+			<li class="user">
+				<div class="anyUserImage"></div>
+				<div class="anyUserName">
+					CarlSax2000
+				</div>
+				<div class="status offline">
+
+				</div>
+			</li>
+			<li class="user">
+				<div class="anyUserImage"></div>
+				<div class="anyUserName">
+					BilardoTech
+				</div>
+				<div class="status offline">
+
+				</div>
+			</li>
+			<li class="user">
+				<div class="anyUserImage"></div>
+				<div class="anyUserName">
+					PacieAug10
+				</div>
+				<div class="status online">
+
+				</div>
+			</li>
+			<li class="user">
+				<div class="anyUserImage"></div>
+				<div class="anyUserName">
+					DaphTech99
+				</div>
+				<div class="status online">
+
+				</div>
+			</li>
+			<li class="user">
+				<div class="anyUserImage"></div>
+				<div class="anyUserName">
+					BeonVae08
+				</div>
+				<div class="status online">
+
+				</div>
+			</li>
+			<li class="user">
+				<div class="anyUserImage"></div>
+				<div class="anyUserName">
+					ChristEti_21
+				</div>
+				<div class="status offline">
+
+				</div>
+			</li>
+			<li class="user">
+				<div class="anyUserImage"></div>
+				<div class="anyUserName">
+					WenChyPrice95
+				</div>
+				<div class="status online">
+
+				</div>
+			</li>
+			<li class="user">
+				<div class="anyUserImage"></div>
+				<div class="anyUserName">
+					LeandreWoodly
+				</div>
+				<div class="status offline">
+
+				</div>
+			</li>
+			<li class="user">
+				<div class="anyUserImage"></div>
+				<div class="anyUserName">
+					Noir_Gatz
+				</div>
+				<div class="status online">
+
+				</div>
+			</li>
+			<li class="user">
+				<div class="anyUserImage"></div>
+				<div class="anyUserName">
+					MonteauDa
+				</div>
+				<div class="status offline">
+
+				</div>
+			</li>
+			<li class="user">
+				<div class="anyUserImage"></div>
+				<div class="anyUserName">
+					Billy124
+				</div>
+				<div class="status online">
+
+				</div>
+			</li>
+		</ul>
+	</aside>
+
+	<section id="mainSection">
+		<header id="headerMain">
+			<h1 id="chatTittle">Tyson9Tech Chat Room</h1>
+		</header>
+		
+		<main id="main"></main>
+
+		<footer id="footerMain">
+			<form id="form">
+				<textarea id="msgText" cols="30" rows="1"></textarea>
+				<button type="submit" id="sndMsg">Send</button>
+			</form>
+		</footer>
+	</section>
+</body>
+
+</html>
